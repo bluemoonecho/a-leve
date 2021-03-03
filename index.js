5a1ae2a9a0e99a30b8ab715e34f9d8bc890e6341
Sabine Holler <sabidasa@gmail.com>
2021-03-03 16:36:10 +0100

    popup image querie

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 $(document).ready(function(){
 
-
 setTimeout(function () {
     document.getElementById('intro-leve').play()
-}, 2000);
+}, 3000);
 
 
 
@@ -13,18 +12,18 @@ setTimeout(function () {
 
 
 
-// var playPromise = document.getElementById('intro-leve').play();
+var playPromise = document.getElementById('intro-leve').play();
 
-// // In browsers that don’t yet support this functionality,
-// // playPromise won’t be defined.
-// if (playPromise !== undefined) {
-//   playPromise.then(function() {
-//     // Automatic playback started!
-//   }).catch(function(error) {
-//     // Automatic playback failed.
-//     // Show a UI element to let the user manually start playback.
-//   });
-// }
+// In browsers that don’t yet support this functionality,
+// playPromise won’t be defined.
+if (playPromise !== undefined) {
+  playPromise.then(function() {
+    // Automatic playback started!
+  }).catch(function(error) {
+    // Automatic playback failed.
+    // Show a UI element to let the user manually start playback.
+  });
+}
 
 
 // Opening page timeout
